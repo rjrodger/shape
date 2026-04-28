2e61302b0593770ed08173fa17384fd97aecbb73
Richard Rodger <richard@ricebridge.com>
2026-04-28 23:51:35 +0100

    v10.1.0

--- src/shape.ts
+++ src/shape.ts
@@ -25,7 +25,7 @@ import { inspect } from 'util'
 
 
 // Package version.
-const VERSION = '10.0.1'
+const VERSION = '10.1.0'
 
 // Unique symbol for marking and recognizing Shape shapes.
 const SHAPE$ = Symbol.for('shape$')
